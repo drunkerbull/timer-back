8f1d45fe46bfb8088cc6f2dffba60a4e0763c4a3
drunkerbull <mvgamesdw@gmail.com>
2019-10-28 11:27:38 +0100

    save stats

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import DB from './db/mongoose';
 import authRouter from './routes/auth';
 import userRouter from './routes/user';
 import taskRouter from './routes/task';
+import statisticsRouter from './routes/statistics';
 import projectRouter from './routes/project';
 import cors from 'cors';
 
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use(authRouter);
 app.use(userRouter);
 app.use(taskRouter);
+app.use(statisticsRouter);
 app.use(projectRouter);
 
 app.use(express.static(__dirname + '/views'));
